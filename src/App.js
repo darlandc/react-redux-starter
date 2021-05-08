99decde218bcfeebab25b9ca9e2faa5f14d0d3eb
Darlan de Castro <darlandc@gmail.com>
2021-05-08 17:00:35 -0300

    wip store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
+import '@dracula/dracula-ui/styles/dracula-ui.css'
+import { Paragraph } from '@dracula/dracula-ui'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Paragraph className="paragraph" color="pinkPurple">Hello Vampire</Paragraph>
 }
 
 export default App;
